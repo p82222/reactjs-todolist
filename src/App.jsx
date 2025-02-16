c9e597a2cd41ea9bce7d156d0235cfee587779b2
Chia-Yu Liu <chia-yuliu@chia-yus-air.home>
2025-02-16 12:17:17 -0500

    Fixed TodoInput.jsx and added Enter Key function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from "./components/TodoList";
 
 function App() {
 
-  let todos =[
-    'Go to the gym',
-    'Buy food',
-    'File the tax'
-  ]
+  const [todos, setTodos] = useState([])
+
+  function updateAddTodos(newTodo){
+
+    const newTodoList = [...todos,newTodo]
+    setTodos(newTodoList)
+
+  }
 
   return (
     <>
       {/* <TodoInput />*/}
-      <TodoInput />
+      <TodoInput updateAddTodos = {updateAddTodos} />
        {/* <TodoList />*/}
        <TodoList todos = {todos}/>
     </>
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function TodoInput() {
+export default function TodoInput({ updateAddTodos }) {
+
+  const [todoValue, setTodoValue] = useState('');
+
+  const handleAddTodo =()=> {
+    if(todoValue.trim() == '') return; //checking if input is empty
+    updateAddTodos(todoValue) //add to todo list
+    setTodoValue('') //clear input field after adding
+  };
+
   return (
     <header>
-      <input placeholder="Entre todo..."/>
-      <button>Add</button>
+      <input 
+      value={todoValue} 
+      onChange={(e) => setTodoValue(e.target.value)}
+      onKeyDown={(e) => {
+        if(e.key == 'Enter') handleAddTodo();
+      }}
+      
+      placeholder="Entre todo..."/>
+
+      <button onClick={handleAddTodo}>Add</button>
     </header>
   )
 }
